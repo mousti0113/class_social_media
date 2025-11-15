545a33902a809ed7f47c522da8856209db4f7950
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2025-11-15 14:56:29 +0100

    refactor: tolto un general-service e rinominato il file che contiene gli interfaces

--- frontend/src/app/services/general-service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class GeneralService {
-  isDarkMode = signal(false); // Segnale per tracciare lo stato
-
-}
