c6fe4f31dc5ce3b57e56c2e8b9f25a9bded1ac9f
Moustapha Mbaye <144794318+mousti0113@users.noreply.github.com>
2025-11-18 08:30:07 +0000

    feat: creato ToastComponent

---
+++ frontend/src/app/shared/ui/toast-component/toast-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-component',
+  imports: [],
+  templateUrl: './toast-component.html',
+  styleUrl: './toast-component.scss',
+})
+export class ToastComponent {
+
+}
