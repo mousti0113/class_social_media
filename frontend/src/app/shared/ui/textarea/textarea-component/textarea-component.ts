555cd1c9cd54a3346be80d604f36bb1856057b6f
Moustapha Mbaye <144794318+mousti0113@users.noreply.github.com>
2025-11-25 07:43:56 +0000

    feat: creato textarea-component

---
+++ frontend/src/app/shared/ui/textarea/textarea-component/textarea-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea-component',
+  imports: [],
+  templateUrl: './textarea-component.html',
+  styleUrl: './textarea-component.scss',
+})
+export class TextareaComponent {
+
+}
