6f1a4bccb17cb9e1a665665752c42fa3d3753516
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2025-12-16 05:44:52 +0100

    fix: aggiorna gli URL API e WebSocket per il corretto funzionamento dell'applicazione

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://concord-eggs-partition-travelers.trycloudflare.com/api',
-  wsUrl: 'https://concord-eggs-partition-travelers.trycloudflare.com/ws',
+  apiUrl: 'https://restaurant-reduces-referrals-burning.trycloudflare.com/api',
+  wsUrl: 'https://restaurant-reduces-referrals-burning.trycloudflare.com/ws',
 
   // Cloudinary Storage Configuration
   cloudinary: {
